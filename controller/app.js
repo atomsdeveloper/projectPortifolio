6c4699002729d201eb56a1e6df972964e83ae429
renannascimentodasilva <atomsfrontend@gmail.com>
2022-10-29 21:05:46 -0300

    Implementação de Cards Animados

--- controller/app.js
+++ controller/app.js
@@ -1,11 +1,21 @@
+// VanillaTilt.init(document.querySelector(".boxCard"), {
+//     max: 25,
+//     speed: 500,
+//     glare: true,
+//     "max-glare": 0.7
+// });
+
+alert(`Em Desenvolvimento!!!`)
+
 const btnMobile = document.getElementById("btn");
 
 const click = () => {
     const list = document.getElementById("menu");
     list.classList.toggle("active");
-
+    
 };
 
 
 
 btnMobile.addEventListener('click', click);
+
